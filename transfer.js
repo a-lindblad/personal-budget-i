a915f9c3cfc3310add7c09039bb7ea01c38d1676
Anders Lindblad <anders.h.lindblad@gmail.com>
2023-08-01 20:44:04 +0200

    Added params handling and basic validaton for all transfer routs

--- transfer.js
+++ transfer.js
@@ -3,6 +3,22 @@ const express = require('express');
 // Instantiate the app here
 const transferRouter = express.Router();
 
+transferRouter.param(['from', 'to'], (req, res, next) => {
+    const source = req.params.from;
+    const dest = req.params.to;
+    if (isNaN(source)) {
+        res.status(400).send();
+        return;
+    }
+    req.source = Number(source);
 
+    if (isNaN(dest)) {
+        res.status(400).send();
+        return;
+    }
+    req.dest = Number(dest);
+
+    next();
+});
 
 module.exports = transferRouter;
